47926da7228db4d2151107de9ab5da79d6b6b0e5
hriday <alauddinhriday@gmail.com>
2024-05-05 15:11:48 +0600

    part1 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import Navbar from './Navbar/Navbar'
+import Part1 from './part1/Part1'
 
 function App() {
 
   return (
     <>
       <Navbar/>
+      <Part1/>
     </>
   )
 }
---
+++ src/part1/Part1.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Part1 = () => {
+  return (
+    <>
+        <div className='flex gap-2 text-white p-4'>
+            <div className='basis-4/6 opacity-80'>
+                <h1 className='text-xl bg-slate-900 p-3 border-b-2'>ALAUDDIN HRIDAY</h1>
+                <p className='bg-slate-900 p-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Nesciunt nihil minima magni quos? Quibusdam corporis
+                    dicta consectetur eveniet vero pariatur?
+                </p>
+            </div>
+            <div className='basis-2/6 bg-slate-300 p-2 hover:bg-orange-500 hover:text-white text-black border-2 border-slate-900 opacity-90'>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Culpa quibusdam tenetur iste maiores aperiam laborum,
+                    amet impedit quia cumque laudantium!
+                </p>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Part1
