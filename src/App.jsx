2c532ccced15205b9ab844f17872618db0407d0e
hriday <alauddinhriday@gmail.com>
2024-05-05 20:56:25 +0600

    part4 added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './Navbar/Navbar'
 import Part1 from './part1/Part1'
 import Part2 from './part2/Part2'
 import Part3 from './part3/Part3'
+import Part4 from './part4.jsx/Part4'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Part1/>
       <Part2/>
       <Part3/>
+      {/* <Part4/> */}
     </>
   )
 }
---
+++ src/part4.jsx/Part4.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Part4.css'
+
+export default function Part4() {
+  return (
+    <>
+        <div className='flex bg-slate-900 border-2 p-2'>
+            <div className='circle-div size-60 bg-blue-500 flex justify-center items-center border-2 rounded-full'>
+                <p className='size-52 bg-slate-900 border-2 rounded-full'></p>
+            </div>
+            <div className='size-4/6 border-2'>
+                <p className=''>Hello World</p>
+            </div>
+        </div>
+    </>
+  )
+}
